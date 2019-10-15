ab00266d66606c5bc8d4dfb7747bd49b1655c729
Nimrod Gaver <gavernimrod@gmail.com>
2019-10-15 11:08:16 +0300

    My first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function toggleArrow(ev) {
+    ev.stopPropagation();
+    var elBtn= ev.fromElement
+    var elArrow = elBtn.querySelector('span.btn-arrow')
+    elArrow.classList.toggle('hide-visibility');
+}
+
+
+function toggleMenu(){
+    var elMenu = document.querySelector('.menu');
+    elMenu.classList.toggle('open')
+    var elMenuBtn = document.querySelector('.menu-mobile-btn')
+    if (elMenu.classList.contains('open')) {
+        elMenuBtn.innerHTML = '<i class="fas fa-times">'
+    } else {
+        elMenuBtn.innerHTML = '<i class="fas fa-bars">'
+    }
+}
